6335fd79153e73f299a559de5e65f29f3b87672c
root <root@gmail.com>
2022-02-26 00:58:21 +0000

    Almost done with typing tutor

--- _02_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
+++ _02_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
@@ -17,28 +17,37 @@ class ChuckleClicker(tk.Tk):
 
         # TODO: Declare and initialize 2 buttons (tk.Button)
         #  one button for the joke and another for the punchline
-
+        self.joke_button = tk.Button(self, text = 'Joke', fg = 'black',
+                                font = ('Times New Roman', 20, 'bold'))
+        self.joke_button.place(relx = 0.02, rely = 0.1, relwidth = 0.5, relheight = 0.8)
+        self.punchline_button = tk.Button(self, text='Punchline', fg='black',
+                                font=('Times New Roman', 20, 'bold'))
+        self.punchline_button.place(relx=0.5, rely=0.1, relwidth=0.5, relheight=0.8)
         # TODO: Place the 2 buttons next to each other (see example image)
 
         # TODO: Call the joke button's bind() method to call the on_joke()
         #  method when a mouse button is pressed
         #  example: self.joke_button.bind('<ButtonPress>', self.on_joke)
-
+        self.joke_button.bind('<ButtonPress>', self.on_joke)
         # TODO: Call the joke button's bind() method to call the on_punchline()
         #  method when a mouse button is pressed
-
+        self.punchline_button.bind('<ButtonPress>', self.on_punchline)
     def on_joke(self, event):
-        print('Joke button pressed')
+        messagebox.showinfo(title = '', message = 'I asked a Frenchman if he played any video games...')
 
         # TODO: Write your joke below!
 
     def on_punchline(self, event):
-        print('Punchline button pressed')
+        messagebox.showinfo(title = '', message = 'He said Wii....')
 
         # TODO: Write a punchline to your joke!
 
 
 if __name__ == '__main__':
+    laugh = ChuckleClicker()
+    laugh.geometry('300x500')
+    laugh.title('Joking')
+    laugh.mainloop()
     pass
     # TODO: Create a new ChuckleClicker object and set the title and geometry.
     #  Remember to call mainloop() at the end
--- _02_gui_app_bindings/_b_TypingTutor/typing_tutor.py
+++ _02_gui_app_bindings/_b_TypingTutor/typing_tutor.py
@@ -15,22 +15,30 @@ class TypingTutor(tk.Tk):
 
         # TODO: Declare and initialize a member variable to hold a
         #  random letter to type
-
+        self.random_letter = generate_random_letter() # 'w'
         # TODO: Declare and initialize a label (tk.Label) and set the text to the random letter
-
+        self.label = tk.Label(self, text = self.random_letter, bg = 'blue', fg = 'black',
+                              font = ('Times New Roman', 20, 'bold'),relief = 'solid')
+        self.label.place(relx = 0.2, rely= 0.2, relwidth = 0.5, relheight = 0.3)
         # TODO: Place the label in the center of the window
-
+        self.label.focus_set()
         # TODO: Call the label's focus_set() method so key presses can be detected
 
         # TODO: Call the label's bind() method to call the on_key_release()
         #  method when a key is released
         #  example: self.label.bind('<KeyRelease>', self.on_key_release)
-
+        self.label.bind('<KeyRelease>', self.on_key_release)
 
     def on_key_release(self, event):
         letter_pressed = event.char
         print('key ' + letter_pressed + ' released!')
+        if letter_pressed == self.random_letter:
+            self.label.configure(bg='green')
+            self.random_letter = generate_random_letter() # 't'
+            self.label.configure(text=self.random_letter)
 
+        else:
+            self.label.configure(bg = 'red')
         # TODO: If the user pressed the correct key,
         #         change the label's background to green
         #       If the user pressed the wrong key,
@@ -42,6 +50,10 @@ class TypingTutor(tk.Tk):
 
 
 if __name__ == '__main__':
+    typing = TypingTutor()
+    typing.title('Typing')
+    typing.geometry('500x500')
+    typing.mainloop()
     pass
     # TODO: Create a new _b_TypingTutor object and set the title and geometry.
     #  Remember to call mainloop() at the end
