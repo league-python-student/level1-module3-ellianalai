55df81fda450457bce063b1441bfb9a5e2f75e8d
root <root@gmail.com>
2022-03-25 23:58:46 +0000

    Working on Jeopardy

--- _02_gui_app_bindings/_e_Jeopardy/jeopardy.py
+++ _02_gui_app_bindings/_e_Jeopardy/jeopardy.py
@@ -14,7 +14,7 @@ class Jeopardy(tk.Tk):
         button_width, button_height, num_buttons = self.setup_buttons(categories)
 
         # TODO: Create a member variable for the list of categories
-        self.category = categories
+        self.categories = categories
         # TODO: Create a member variable for the score/money
         self.score= 0
         for i in range(num_buttons):
@@ -27,8 +27,7 @@ class Jeopardy(tk.Tk):
             # Create the category header and buttons where
             # row 0 is the category title
             if row_num == 0:
-                category.name() #I still don't get what this really means
-                self.label = tk.Label(self, )
+                self.label = tk.Label(self,text = category.name )
                 self.label.place(x = col_x, y = row_y, width = button_width, height = button_height)
                 pass
                 # TODO: To get the category name, use the categories member variable and column num
@@ -40,7 +39,7 @@ class Jeopardy(tk.Tk):
                 value = category.questions[row_num - 1].value
 
                 # TODO: Create a tk.Button with the questions' value on the button
-                self.button=tk.Button(self,) #do I add anything else?
+                self.button=tk.Button(self, text = value) #do I add anything else?
                 self.button.place(x=col_x, y=row_y, width=button_width, height=button_height)
                 # TODO: Place the Button using the 'col_x', 'row_y', 'button_width',
                 #  and 'button_height' variables
@@ -50,14 +49,14 @@ class Jeopardy(tk.Tk):
                 self.button.bind('<ButtonPress>', self.on_button_press)
 
                 # TODO: Add the button to the category's list of buttons
-                self.category.append(self.button)
+                category.buttons.append(self.button)
 
     def on_button_press(self, event):
         button_pressed = event.widget
         print('button ' + repr(button_pressed) + ' clicked!')
-
+        self.ask_question(button_pressed)
         # TODO: Call the ask_question() method with button_pressed as an input
-        ask_question(button_pressed) #what am I supposed to do??
+
     def ask_question(self, button_pressed):
 
         for category in self.categories:
@@ -74,7 +73,7 @@ class Jeopardy(tk.Tk):
                         #  the question and get their response. If their response is correct,
                         #  increase the score member variable by the value. Otherwise, subtract
                         #  value from the score
-
+    #DO THIS NEXT!!
 
     def setup_buttons(self, categories):
         # Window size needs to be updated immediately here so the
@@ -122,10 +121,20 @@ if __name__ == '__main__':
 
     # TODO: Use the Category class above to create at least 3 question categories
     #  for your _e_Jeopardy game
-
+    science = Category('Science')
+    science.add_question('Is baking a cake a physical or chemical change?', 'Chemical Change', 2)
+    history = Category('History')
+    history.add_question('What warriors followed the Bushido code?', 'Samurai', 3)
+    math = Category('Math')
+    math.add_question('What is 1+1', 'Window', 1)
+    english = Category('English')
+    english.add_question('What defines a dependent clause', 'Starts with a subordinator', 5)
     # TODO: For each Category, use the add_question method to add a question, answer, and
     #  a value for each question
-
+    j_categories.append(science)
+    j_categories.append(history)
+    j_categories.append(math)
+    j_categories.append(english)
     game = Jeopardy(j_categories)
     game.title('_e_Jeopardy')
     game.mainloop()
